388f7b7fb2c275715b1da8df9b6c7a3a526d8364
MANOJ KUMAR MITHIREDDY <148680918+ManojKumarMithireddy@users.noreply.github.com>
2024-10-11 13:56:06 +0000

    dummy

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import styles from './page.module.css';
 export default function Home() {
   return (
-    // style tag
+
     <>
       <style jsx>{`
         .form-container {
